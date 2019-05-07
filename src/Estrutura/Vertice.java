881a21aee76ce849ff0e21c1d335d35f7d7cf04e
Paulo Ezequiel <paulo@inventione.com.br>
2019-05-07 14:18:50 -0300

    adicionando estrutura do grafo

---
+++ src/Estrutura/Vertice.java
@@ -0,0 +1,25 @@
+package Estrutura;
+
+
+public class Vertice<K extends Comparable<? super K>, V> {
+
+	K Label;
+	V val;
+	int index;
+    boolean foiVisitado;
+    int numAdjacente;
+    int colorido;
+	public Vertice(K label, V val,int index) {
+		Label = label;
+		this.val = val;
+		this.index=index;
+		this.foiVisitado=false;
+		this.numAdjacente=0;
+	}
+	
+	public void IncNumAdj() {numAdjacente++;}
+		
+	public void DecNumAdj() {numAdjacente--;}
+	
+	public int getNumAdj() {return numAdjacente;}
+}
--- src/teste.java
+++ src/teste.java
@@ -1,8 +1,30 @@
+import Estrutura.Grafo;
 
 public class teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Grafo<Integer, String> g = new Grafo<Integer, String>();
+
+		g.AddVertice(0, "Paulo");
+		g.AddVertice(1, "Lucas");
+		g.AddVertice(2, "Vera");
+		g.AddVertice(3, "Vitória");
+		g.AddVertice(4, "Marcos");
+
+		g.AddAresta(0, 4, 1);
+		g.AddAresta(4, 1, 1);
+		g.AddAresta(4, 2, 1);
+		g.AddAresta(4, 3, 1);
+		g.AddAresta(1, 2, 1);
+		g.AddAresta(2, 3, 1);
+
+		g.ExibirAdj(1);
+		// System.out.println(g.ehConexo(1));
+
+		// System.out.print(g.ehCompleto());
+
+		
+		// c.ExibirPorNumAdj();
 
 	}
 
